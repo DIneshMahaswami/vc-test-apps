0cc225029957f9699db00bcc7828adb13d6e15a2
Dinesh <dinesh@mahaswami.com>
2026-06-03 16:58:00 +0530

    Test app 2 commit

---
+++ core-builders/src/views/customers.tsx
@@ -0,0 +1,260 @@
+import { Resource, createDefaults, tableDefaults,
+	editDefaults, formDefaults, tabbedFormDefaults, layoutDefaults, listDefaults, showDefaults,
+	RowActions, DataTable, WizardForm, createReferenceField, createReferenceInput,
+	type ResourceActionDefs, type FieldSchema, SimpleShowLayout, SimpleFileField, SimpleFileInput, CardGrid, ChoicesLiveFilter, TextLiveFilter} from '@mahaswami/vc-frontend';
+import { People } from '@mui/icons-material';
+import { Create, Edit, List, Menu, Show,
+    TabbedForm, TabbedShowLayout,
+    type ListProps, TextField, TextInput, SelectField, SelectInput, required, useUnique} from "react-admin";
+import { Box } from '@mui/material';
+
+export const RESOURCE = "customers"
+export const ICON = People
+export const PREFETCH: string[] = []
+
+export const CustomersReferenceField = createReferenceField(RESOURCE, PREFETCH);
+export const CustomersReferenceInput = createReferenceInput(RESOURCE, PREFETCH);
+const customersActionDefs: ResourceActionDefs = {};
+
+export const businessTypeChoices = [{ id: 'it_services', name: 'IT Services' }, { id: 'retail', name: 'Retail' }, { id: 'manufacturing', name: 'Manufacturing' }, { id: 'healthcare', name: 'Healthcare' }, { id: 'education', name: 'Education' }, { id: 'consulting', name: 'Consulting' }];
+export const BusinessTypeChoiceField = (props: any) => <SelectField {...props} choices={businessTypeChoices} />;
+
+const filters = [
+    <TextLiveFilter source="search" show />,
+    <ChoicesLiveFilter source="business_type" label="Business Type" choiceLabels={businessTypeChoices} show />
+]
+
+export const CustomersList = (props: ListProps) => {
+    return (
+        <List {...listDefaults(props)}>
+            <DataTable {...tableDefaults(RESOURCE)} hiddenColumns={['primary_contact_phone', 'primary_contact_email', 'secondary_contact_name', 'secondary_contact_phone', 'secondary_contact_email', 'registered_building_name', 'registered_suite', 'registered_address_line_1', 'registered_address_line_2', 'registered_city', 'registered_state', 'registered_pin_code']} >
+                <DataTable.Col source="business_name" />
+                <DataTable.Col source="business_type" field={BusinessTypeChoiceField} />
+                <DataTable.Col source="business_pan" />
+                <DataTable.Col source="gstin" />
+                <DataTable.Col source="primary_contact_name" />
+                <DataTable.Col source="primary_contact_phone" />
+                <DataTable.Col source="primary_contact_email" />
+                <DataTable.Col source="secondary_contact_name" />
+                <DataTable.Col source="secondary_contact_phone" />
+                <DataTable.Col source="secondary_contact_email" />
+                <DataTable.Col source="registered_building_name" />
+                <DataTable.Col source="registered_suite" />
+                <DataTable.Col source="registered_address_line_1" />
+                <DataTable.Col source="registered_address_line_2" />
+                <DataTable.Col source="registered_city" />
+                <DataTable.Col source="registered_state" />
+                <DataTable.Col source="registered_pin_code" />
+                <RowActions/>
+            </DataTable>
+        </List>
+    )
+}
+
+export const CustomersCardList = (props: ListProps) => {
+    return (
+        <List {...listDefaults(props)} component={'div'}>
+            <CardGrid title={<TextField source="business_name" variant='h6' />}>
+                <SelectField source="business_type" choices={businessTypeChoices} />
+                <TextField source="business_pan" />
+            </CardGrid>
+        </List>
+    )
+}
+
+const BusinessInformationInputs = () => {
+    const unique = useUnique();
+    return (
+        <>
+            <TextInput source="business_name" validate={required()} />
+            <SelectInput source="business_type" choices={businessTypeChoices} validate={required()} />
+            <TextInput source="business_pan" validate={[required(), unique()]} />
+            <TextInput source="gstin" validate={[required(), unique()]} />
+        </>
+    )
+}
+
+const ContactsInputs = () => {
+    const unique = useUnique();
+    return (
+        <>
+            <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', md: '1fr 1fr' }, columnGap: '1em', width: '100%' }}>
+                <TextInput source="primary_contact_name" validate={required()} />
+                <TextInput source="primary_contact_phone" validate={required()} />
+                <TextInput source="primary_contact_email" validate={[required(), unique()]} />
+                <TextInput source="secondary_contact_name" />
+                <TextInput source="secondary_contact_phone" />
+                <TextInput source="secondary_contact_email" />
+            </Box>
+        </>
+    )
+}
+
+const AddressDocumentsInputs = () => {
+    return (
+        <>
+            <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', md: '1fr 1fr' }, columnGap: '1em', width: '100%' }}>
+                <TextInput source="registered_building_name" />
+                <TextInput source="registered_suite" />
+                <TextInput source="registered_address_line_1" />
+                <TextInput source="registered_address_line_2" />
+                <TextInput source="registered_city" />
+                <TextInput source="registered_state" />
+                <TextInput source="registered_pin_code" />
+                <SimpleFileInput source="incorporation_certificate_attachment_file_id" />
+                <SimpleFileField source="incorporation_certificate_attachment_file_id" title="incorporation_certificate_attachment_file_name" />
+                <SimpleFileInput source="memorandum_of_articles_attachment_file_id" />
+                <SimpleFileField source="memorandum_of_articles_attachment_file_id" title="memorandum_of_articles_attachment_file_name" />
+                <SimpleFileInput source="articles_of_association_attachment_file_id" />
+                <SimpleFileField source="articles_of_association_attachment_file_id" title="articles_of_association_attachment_file_name" />
+                <SimpleFileInput source="gst_registration_attachment_file_id" />
+                <SimpleFileField source="gst_registration_attachment_file_id" title="gst_registration_attachment_file_name" />
+            </Box>
+        </>
+    )
+}
+
+const CustomerEdit = (props: any) => {
+    return (
+        <Edit {...editDefaults(props)}>
+            <TabbedForm {...tabbedFormDefaults(props)}>
+                <TabbedForm.Tab label="Business Information">
+                    <BusinessInformationInputs/>
+                </TabbedForm.Tab>
+                <TabbedForm.Tab label="Contacts">
+                    <ContactsInputs/>
+                </TabbedForm.Tab>
+                <TabbedForm.Tab label="Address & Documents">
+                    <AddressDocumentsInputs/>
+                </TabbedForm.Tab>
+            </TabbedForm>
+        </Edit>
+    )
+}
+
+const CustomerCreate = (props: any) => {
+    return (
+        <Create {...createDefaults(props)}>
+            <WizardForm>
+                <WizardForm.Step label='Business Information'>
+                    <BusinessInformationInputs/>
+                </WizardForm.Step>
+                <WizardForm.Step label='Contacts'>
+                    <ContactsInputs/>
+                </WizardForm.Step>
+                <WizardForm.Step label='Address & Documents'>
+                    <AddressDocumentsInputs/>
+                </WizardForm.Step>
+            </WizardForm>
+        </Create>
+    )
+}
+
+const CustomerShow = (props: any) => {
+    return (
+        <Show {...showDefaults(props)}>
+            <TabbedShowLayout {...layoutDefaults(props)}>
+                <TabbedShowLayout.Tab label="Business Information">
+                    <TextField source="business_name" />
+                    <SelectField source="business_type" choices={businessTypeChoices} />
+                    <TextField source="business_pan" />
+                    <TextField source="gstin" />
+                </TabbedShowLayout.Tab>
+                <TabbedShowLayout.Tab label="Contacts">
+                    <SimpleShowLayout display={'grid'} gridTemplateColumns={{ xs: '1fr', md: '1fr 1fr' }}>
+                        <TextField source="primary_contact_name" />
+                        <TextField source="primary_contact_phone" />
+                        <TextField source="primary_contact_email" />
+                        <TextField source="secondary_contact_name" />
+                        <TextField source="secondary_contact_phone" />
+                        <TextField source="secondary_contact_email" />
+                    </SimpleShowLayout>
+                </TabbedShowLayout.Tab>
+                <TabbedShowLayout.Tab label="Address & Documents">
+                    <SimpleShowLayout display={'grid'} gridTemplateColumns={{ xs: '1fr', md: '1fr 1fr' }}>
+                        <TextField source="registered_building_name" />
+                        <TextField source="registered_suite" />
+                        <TextField source="registered_address_line_1" />
+                        <TextField source="registered_address_line_2" />
+                        <TextField source="registered_city" />
+                        <TextField source="registered_state" />
+                        <TextField source="registered_pin_code" />
+                        <SimpleFileField source="incorporation_certificate_attachment_file_id" title="incorporation_certificate_attachment_file_name" />
+                        <SimpleFileField source="memorandum_of_articles_attachment_file_id" title="memorandum_of_articles_attachment_file_name" />
+                        <SimpleFileField source="articles_of_association_attachment_file_id" title="articles_of_association_attachment_file_name" />
+                        <SimpleFileField source="gst_registration_attachment_file_id" title="gst_registration_attachment_file_name" />
+                    </SimpleShowLayout>
+                </TabbedShowLayout.Tab>
+            </TabbedShowLayout>
+        </Show>
+    )
+}
+
+const customersFieldSchema: FieldSchema = {
+    business_name: { required: true },
+    business_type: { ui: 'select', required: true, choices: businessTypeChoices },
+    business_pan: { required: true, unique: true },
+    gstin: { required: true, unique: true },
+    primary_contact_name: { required: true },
+    primary_contact_phone: { required: true },
+    primary_contact_email: { required: true, unique: true },
+    secondary_contact_name: {},
+    secondary_contact_phone: {},
+    secondary_contact_email: {},
+    registered_building_name: {},
+    registered_suite: {},
+    registered_address_line_1: {},
+    registered_address_line_2: {},
+    registered_city: {},
+    registered_state: {},
+    registered_pin_code: {},
+    incorporation_certificate_attachment_file_id: {},
+    memorandum_of_articles_attachment_file_id: {},
+    articles_of_association_attachment_file_id: {},
+    gst_registration_attachment_file_id: {}
+};
+const customersSearchableFields: string[] = [
+    'business_name',
+    'primary_contact_name',
+    'secondary_contact_name',
+    'registered_building_name',
+    'business_type',
+    'business_pan',
+    'gstin',
+    'primary_contact_phone',
+    'primary_contact_email',
+    'secondary_contact_phone',
+    'secondary_contact_email',
+    'registered_suite',
+    'registered_address_line_1',
+    'registered_address_line_2',
+    'registered_city',
+    'registered_state',
+    'registered_pin_code'
+];
+
+export const CustomersResource = (
+    <Resource
+        name={RESOURCE}
+        icon={ICON}
+        prefetch={PREFETCH}
+        recordRepresentation={(record: any) => record.business_name}
+        fieldSchema={ customersFieldSchema}
+        actionDefs={ customersActionDefs}
+        searchableFields={ customersSearchableFields}
+        filters={filters}
+        filtersPlacement="top"
+        list={<CustomersList/>}
+        create={<CustomerCreate/>}
+        edit={<CustomerEdit/>}
+        show={<CustomerShow/>}
+        hasDialog
+        hasLiveUpdate
+        cardList={<CustomersCardList/>}
+        hasColumnChooser
+        sort={{ field: 'business_name', order: 'ASC' }}
+    />
+)
+export const CustomersMenu = () => (
+    <Menu.Item to={`/${RESOURCE}`} primaryText="Customers" leftIcon={<ICON />} />
+)
---
+++ core-builders/src/views/invoices.tsx
@@ -0,0 +1,172 @@
+import { Resource, createDefaults, tableDefaults,
+	editDefaults, formDefaults, listDefaults,
+	showDefaults, RowActions, DataTable, SimpleShowLayout, SimpleForm,
+	type ResourceActionDefs, type FieldSchema, MoneyField, MoneyInput, SimpleFileField, SimpleFileInput, AutoReferenceNumberInput, CardGrid, createReferenceField, createReferenceInput, ReferenceLiveFilter, ChoicesLiveFilter, DateLiveFilter, MoneyLiveFilter, TextLiveFilter} from '@mahaswami/vc-frontend';
+import { Receipt } from '@mui/icons-material';
+import { Create, Edit, List, Menu, Show,
+    type ListProps, TextField, DateField, DateInput, SelectField, SelectInput, AutocompleteInput, required} from "react-admin";
+import { RentalAgreementsReferenceField, RentalAgreementsReferenceInput } from './rental_agreements.js';
+
+export const RESOURCE = "invoices"
+export const ICON = Receipt
+export const PREFETCH: string[] = ["rental_agreements"]
+
+export const InvoicesReferenceField = createReferenceField(RESOURCE, PREFETCH);
+export const InvoicesReferenceInput = createReferenceInput(RESOURCE, PREFETCH);
+const invoicesActionDefs: ResourceActionDefs = {};
+
+export const invoiceTypeChoices = [{ id: 'rent', name: 'Rent' }, { id: 'maintenance', name: 'Maintenance' }, { id: 'security_deposit', name: 'Security Deposit' }, { id: 'late_fee', name: 'Late Fee' }];
+export const InvoiceTypeChoiceField = (props: any) => <SelectField {...props} choices={invoiceTypeChoices} />;
+export const paymentStatusChoices = [{ id: 'pending', name: 'Pending' }, { id: 'partially_paid', name: 'Partially Paid' }, { id: 'overdue', name: 'Overdue' }, { id: 'paid', name: 'Paid' }];
+export const PaymentStatusChoiceField = (props: any) => <SelectField {...props} choices={paymentStatusChoices} />;
+
+export const BaseAmountMoneyField = (props: any) => <MoneyField {...props} currency="USD" />;
+export const GstAmountMoneyField = (props: any) => <MoneyField {...props} currency="USD" />;
+export const TotalAmountMoneyField = (props: any) => <MoneyField {...props} currency="USD" />;
+
+const filters = [
+    <TextLiveFilter source="search" show />,
+    <DateLiveFilter source="invoice_date" label="Invoice" />,
+    <ChoicesLiveFilter source="invoice_type" label="Invoice Type" choiceLabels={invoiceTypeChoices} show />,
+    <ReferenceLiveFilter source="rental_agreement_id" reference="rental_agreements" label="Rental Agreement" />,
+    <ChoicesLiveFilter source="payment_status" label="Payment Status" choiceLabels={paymentStatusChoices} show />,
+    <DateLiveFilter source="due_date" label="Due" />,
+    <MoneyLiveFilter source="base_amount" label="Base" currency="USD" />,
+    <MoneyLiveFilter source="gst_amount" label="Gst" currency="USD" />,
+    <MoneyLiveFilter source="total_amount" label="Total" currency="USD" />
+]
+
+export const InvoicesList = (props: ListProps) => {
+    return (
+        <List {...listDefaults(props)}>
+            <DataTable {...tableDefaults(RESOURCE)} hiddenColumns={['due_date', 'base_amount', 'gst_amount', 'total_amount']} >
+                <DataTable.Col source="invoice_no" />
+                <DataTable.Col source="invoice_date" field={DateField}/>
+                <DataTable.Col source="invoice_type" field={InvoiceTypeChoiceField} />
+                <DataTable.Col source="rental_agreement_id" field={RentalAgreementsReferenceField}/>
+                <DataTable.Col source="payment_status" field={PaymentStatusChoiceField} />
+                <DataTable.Col source="due_date" field={DateField}/>
+                <DataTable.Col source="base_amount" field={BaseAmountMoneyField}/>
+                <DataTable.Col source="gst_amount" field={GstAmountMoneyField}/>
+                <DataTable.Col source="total_amount" field={TotalAmountMoneyField}/>
+                <RowActions/>
+            </DataTable>
+        </List>
+    )
+}
+
+export const InvoicesCardList = (props: ListProps) => {
+    return (
+        <List {...listDefaults(props)} component={'div'}>
+            <CardGrid title={<TextField source="invoice_no" variant='h6' />}>
+                <DateField source="invoice_date" />
+                <SelectField source="invoice_type" choices={invoiceTypeChoices} />
+            </CardGrid>
+        </List>
+    )
+}
+
+const InvoiceForm = (props: any) => {
+    return (
+        <SimpleForm {...formDefaults(props)} display="grid"  gridTemplateColumns={{ xs: '1fr', md: '1fr 1fr' }}  gap="1rem" >
+            <AutoReferenceNumberInput source="invoice_no" format="INV-NNNN" />
+            <DateInput source="invoice_date" validate={required()} />
+            <SelectInput source="invoice_type" choices={invoiceTypeChoices} validate={required()} />
+            <RentalAgreementsReferenceInput source="rental_agreement_id">
+                <AutocompleteInput validate={required()} />
+            </RentalAgreementsReferenceInput>
+            <SelectInput source="payment_status" choices={paymentStatusChoices} validate={required()} />
+            <DateInput source="due_date" validate={required()} />
+            <MoneyInput source="base_amount" currency="USD" />
+            <MoneyInput source="gst_amount" currency="USD" />
+            <MoneyInput source="total_amount" currency="USD" validate={required()} />
+            <SimpleFileInput source="invoice_attachment_file_id" />
+            <SimpleFileField source="invoice_attachment_file_id" title="invoice_attachment_file_name" />
+        </SimpleForm>
+    )
+}
+
+const InvoiceEdit = (props: any) => {
+    return (
+        <Edit {...editDefaults(props)}>
+            <InvoiceForm />
+        </Edit>
+    )
+}
+
+const InvoiceCreate = (props: any) => {
+    return (
+    	<Create {...createDefaults(props)}>
+            <InvoiceForm />
+        </Create>
+    )
+}
+
+const InvoiceShow = (props: any) => {
+    return (
+        <Show {...showDefaults(props)}>
+            <SimpleShowLayout display="grid"  gridTemplateColumns={{ xs: '1fr', md: '1fr 1fr' }}  gap="1rem" >
+                <TextField source="invoice_no" />
+                <DateField source="invoice_date" />
+                <SelectField source="invoice_type" choices={invoiceTypeChoices} />
+                <RentalAgreementsReferenceField source="rental_agreement_id" />
+                <SelectField source="payment_status" choices={paymentStatusChoices} />
+                <DateField source="due_date" />
+                <MoneyField source="base_amount" currency="USD" />
+                <MoneyField source="gst_amount" currency="USD" />
+                <MoneyField source="total_amount" currency="USD" />
+                <SimpleFileField source="invoice_attachment_file_id" title="invoice_attachment_file_name" />
+            </SimpleShowLayout>
+        </Show>
+    )
+}
+
+const invoicesFieldSchema: FieldSchema = {
+    invoice_no: { required: true, autoAssign: { format: 'INV-NNNN' } },
+    invoice_date: { required: true },
+    invoice_type: { ui: 'select', required: true, choices: invoiceTypeChoices },
+    rental_agreement_id: { required: true, resource: 'rental_agreements' },
+    payment_status: { ui: 'select', required: true, choices: paymentStatusChoices },
+    due_date: { required: true },
+    base_amount: { type: 'money', currency: 'USD' },
+    gst_amount: { type: 'money', currency: 'USD' },
+    total_amount: { type: 'money', currency: 'USD', required: true },
+    invoice_attachment_file_id: {}
+};
+const invoicesSearchableFields: string[] = [
+    'invoice_no',
+    'rental_agreement.unit.name',
+    'rental_agreement.customer.business_name',
+    'rental_agreement.customer.primary_contact_name',
+    'rental_agreement.customer.secondary_contact_name',
+    'rental_agreement.customer.registered_building_name',
+    'invoice_type',
+    'payment_status'
+];
+
+export const InvoicesResource = (
+    <Resource
+        name={RESOURCE}
+        icon={ICON}
+        prefetch={PREFETCH}
+        recordRepresentation={(record: any) => record.invoice_no}
+        fieldSchema={ invoicesFieldSchema}
+        actionDefs={ invoicesActionDefs}
+        searchableFields={ invoicesSearchableFields}
+        filters={filters}
+        filtersPlacement="top"
+        list={<InvoicesList/>}
+        create={<InvoiceCreate/>}
+        edit={<InvoiceEdit/>}
+        show={<InvoiceShow/>}
+        hasDialog
+        hasLiveUpdate
+        hasFilterChooser
+        cardList={<InvoicesCardList/>}
+        hasColumnChooser
+        sort={{ field: 'invoice_no', order: 'ASC' }}
+    />
+)
+export const InvoicesMenu = () => (
+    <Menu.Item to={`/${RESOURCE}`} primaryText="Invoices" leftIcon={<ICON />} />
+)
